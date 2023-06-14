4bee23802ce69508a7f5224d67840969f01809fe
RikiAmar <riki.sh700@gmail.com>
2023-06-14 10:03:39 +0300

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,32 @@
 import express from 'express';
 import data from './data.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoutes.js';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use('/api/v1/seed', seedRouter);
+
+app.use(cors());
 
 //Endpoints
 app.get('/api/v1/products', (req, res) => {
   res.send(data.products);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB !');
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Could not connect to MongoDB! ${err.message}`); 
+  });  
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,26 +1,70 @@
 import { Link } from 'react-router-dom';
 import data from '../data';
+import { useEffect, useReducer, useState } from 'react';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'GET_REQUEST':
+      return { ...state, loading: true };
+    case 'GET_SUCCESS':
+      return { ...state, loading: false, products: action.payload };
+    case 'GET_FAILURE':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function HomePage() {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
+    error: '',
+    products: [],
+  });
+
+  useEffect(() => {
+    const getProducts = async () => {
+      dispatch({ type: 'GET_REQUEST' });
+      try {
+        const res = await axios.get('/api/v1/products'); //try catch
+        dispatch({ type: 'GET_SUCCESS', payload: res.data });
+      } catch (error) {
+        dispatch({ type: 'GET_FAILURE', payload: error.message });
+      }
+    };
+
+    getProducts();
+  }, []);
+
   return (
     <div>
-        <h1>Products</h1>
-        <div className="products">
-        {data.products.map(product => (
+      <h1>Products</h1>
+      <div className="products">
+        {
+       loading ? <p>Loading...</p>
+       :
+       error ? (<p>{error}</p>)
+       :
+       (
+        products.map((product) => (
           <div key={product.token} className="product">
             <Link to={`/product/${product.token}`}>
-            <img alt={product.name} src={product.image}></img>
+              <img alt={product.name} src={product.image}></img>
             </Link>
             <div className="product-desc">
-            <Link to={`/product/${product.token}`}>
-            <p>{product.name}</p>
-            </Link>
-            <p><strong>{product.price}$</strong></p>
-            <button>Add to cart</button>
+              <Link to={`/product/${product.token}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>{product.price}$</strong>
+              </p>
+              <button>Add to cart</button>
             </div>
           </div>
-        ))}
-        </div>
+        )))}        
+      </div>
     </div>
   );
 }
